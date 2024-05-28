74ca3f13a9c650cf300e4b47175b776cd1bb0acf
Matheus Dias <matheus.vex@gmail.com>
2024-05-28 18:38:14 -0300

    feat: new layout finish

--- src/components/Main.component.tsx
+++ src/components/Main.component.tsx
@@ -1,4 +1,5 @@
 import myPicture from '../images/my-picture.jpg'
+import arrowWhite  from '../assets/arrow-white.svg'
 
 import { Component } from 'react';
 
@@ -10,13 +11,14 @@ export class Main extends Component {
         <div className='mr-auto place-self-center lg:col-span-7 order-2 lg:order-1'>
           <h2 className='max-w-2xl mb-4 text-xl font-extrabold tracking-tight dark:text-white'>Matheus de Lara Dias da Silva</h2>
           <h3 className='max-w-2xl mb-4 text-xl font-extrabold tracking-tight dark:text-white'>Especialista em desenvolvimento de sistemas</h3>
-          <p className='max-w-2xl mb-8 font-light text-gray-500 lg:mb-6 md:text-lg lg:text-xl dark:text-gray-400'>Trabalhei em projetos de pequeno, médio e grande porte. Te ajudo a pensar e sua soluçao personalizada end to end.</p>
-          <a className='border-2 inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-gray-900 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900'>
+          <p className='max-w-2xl mb-8 font-light text-gray-500 lg:mb-6 md:text-lg lg:text-xl dark:text-gray-400'>Trabalhei em projetos de pequeno, médio e grande porte. Te ajudo a pensar em uma solução personalizada end to end.</p>
+          <a href="mailto:matheus.vex@gmail.com?subject=Assunto do email&body=Ola, gostaria de solicitar um orçamento." className='border-2 inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-gray-900 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900 cursor-pointer'>
             Entre em contato comigo 
+            <img src={ arrowWhite } alt='Nestjs' className='ml-3 cursor-pointer w-5 h-5 mr-4 container-links' />
           </a>
         </div>
         <div className='lg:mt-0 lg:col-span-5 lg:flex mx-auto order-1 lg:order-2'>
-          <img src={ myPicture } alt='imagem' className='rounded-full w-96 h-96' />
+          <img src={ myPicture } alt='imagem' className='rounded-full w-96 h-96 mb-4' />
         </div>
       </div> 
     )
